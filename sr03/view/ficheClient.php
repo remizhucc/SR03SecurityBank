5af8a8f8613c9ca3b9203c30aec0f90c836ef377
Evil Sheep <18717776983@163.com>
2019-03-31 16:25:44 +0200

    page all client

--- sr03/view/ficheClient.php
+++ sr03/view/ficheClient.php
@@ -1,40 +1,51 @@
 <!doctype html>
 <html lang="fr">
 <head>
-  <meta charset="utf-8">
-  <title>Clients</title>
-  <link rel="stylesheet" type="text/css" media="all"  href="/public/css/mystyle.css" />
+    <meta charset="utf-8">
+    <title>Clients</title>
+    <link rel="stylesheet" type="text/css" media="all" href="/public/css/mystyle.css"/>
 </head>
 <body>
-  <header>
+<header>
     <h1>Liste clients</h1>
-  </header>
-  <main>
+</header>
+<main>
     <article>
-      <header>
-        <h2>Bienvenue <?php echo $_SESSION["connected_user"]["prenom"];?> <?php echo $_SESSION["connected_user"]["nom"];?></h2>
-      </header>
-      <div class="liste">
-          <table>
-            <?php
-            foreach ($listeUsers as $cle => $unUser) {
-              echo '<tr>';
-              echo '<td>'.$unUser['nom'].'</td>';
-              echo '<td>'.$unUser['prenom'].'</td>';
-              echo '<td>'.$unUser['numero_compte'].'</td>';
-              echo '</tr>';
-            }
+        <header>
+            <h2>
+                Bienvenue <?php echo $_SESSION["connected_user"]["prenom"]; ?> <?php echo $_SESSION["connected_user"]["nom"]; ?></h2>
+        </header>
+        <div class="liste">
+            <table>
+                <br>
+                <?php
+                foreach ($listeUsers as $cle => $unUser) {
+                    echo '<tr>';
+                    echo '<td>' . $unUser['nom'] . '</td>';
+                    echo '<td>' . $unUser['prenom'] . '</td>';
+                    echo '<td>' . $unUser['numero_compte'] . '</td>';
+                    echo '<td>' .
+                        '<form method="get">'.
+                        '<input type="hidden" name="action" value="virement">'.
+                        '<input type="hidden" name="compteDebite" value='.$unUser["numero_compte"].'>'.
+                        '<button>virement</button>'.
+                        '</form> '.
+                        '</td>';
+                    echo '</tr>';
+                }
 
-             ?>
-          </table>
-      </div>
-      <div class="form">
-        <form method="GET">
-          <input type="hidden" name="action" value="home">
-          <button>Accueil</button>
-        </form>
-      </div>
+                ?>
+
+            </table>
+            <br>
+        </div>
+        <div class="form">
+            <form method="GET">
+                <input type="hidden" name="action" value="home">
+                <button>Accueil</button>
+            </form>
+        </div>
     </article>
-  </main>
+</main>
 </body>
 </html>
